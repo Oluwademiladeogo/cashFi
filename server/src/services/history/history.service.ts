d8d0936accae238167a1d8aea362d4191575f201
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-11-03 08:35:07 +0100

    mod before graphql

--- server/src/services/history/history.service.ts
+++ server/src/services/history/history.service.ts
@@ -1,11 +1,23 @@
-import { Injectable } from '@nestjs/common';
-
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { userModel } from 'models/userschema';
 @Injectable()
 export class HistoryService {
-    getHistory(user){
-
+  async getHistory(id) {
+    const user =  await userModel.findOne({ id: id });
+    if(!user) throw new HttpException("User not found", HttpStatus.BAD_REQUEST)
+    return user.history
+  }
+  async insertHistory(id, message: string) {
+    try{
+        const user = await userModel.findOne({ id: id });
+    const history = user.history;
+    history.push(message);
+    user.history = history;
+    await user.save();
+    return 'done';
     }
-    insertHistory(user, message:string){
-        // findbyidandupdatetransachistory 
+    catch(error){
+        return error;
     }
+  }
 }

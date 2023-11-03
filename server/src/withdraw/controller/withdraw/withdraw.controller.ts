c1de2f0aeedcb4537fa2e636dc6ec19517364d5b
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-11-03 08:35:37 +0100

    mod before graphql

--- server/src/withdraw/controller/withdraw/withdraw.controller.ts
+++ server/src/withdraw/controller/withdraw/withdraw.controller.ts
@@ -19,29 +19,21 @@ export class WithdrawController {
   constructor(
     private usersService: UsersService,
     private withdrawService: WithdrawService,
-    private historyService: HistoryService
+    private historyService: HistoryService,
   ) {}
   @UsePipes(new ValidationPipe())
   @Post()
   async doWithdraw(@Body() res: Response, data: withdrawMoneyDto) {
-    //if we weren't so pressed for time, I should get another table, more secure to store usr bank, wallet details then link them to regular user table
-    //Auth Middleware used
-    const user = await this.usersService.getUser(data.email)
+    const user = await this.usersService.getUser(data.email);
     if (!user) return res.json('User not found');
-    //Scrapped out for simplicity
-    // if (
-    //   !this.withdrawService.getBankandAccount(data.accountNumber, data.bankName)
-    // )
-    //   throw new HttpException('Account not found', HttpStatus.BAD_REQUEST);
-    const result = await compare(data.pin, user.pin)
-    if (result)
-      throw new HttpException('Invalid pin', HttpStatus.BAD_REQUEST);
+    const result = await compare(data.pin, user.pin);
+    if (result) throw new HttpException('Invalid pin', HttpStatus.BAD_REQUEST);
     const aggBal = user.balance + 1000;
     if (data.amount > aggBal)
       throw new HttpException('insufficient funds', HttpStatus.BAD_REQUEST);
-    const amount = data.amount - user.balance
-    this.usersService.updateUserBalance(user.id, amount);
-    const message = ""
-    this.historyService.insertHistory(user, message)
+    const amount = data.amount - user.balance;
+    await this.usersService.updateUserBalance(user.id, amount);
+    const message = '';
+    await this.historyService.insertHistory(user.id, message);
   }
 }

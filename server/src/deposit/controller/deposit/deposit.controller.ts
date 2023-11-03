9aa532c07d1efa76f54986775ae7b231fb4a1823
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-11-03 08:31:58 +0100

    mod before graphql

--- server/src/deposit/controller/deposit/deposit.controller.ts
+++ server/src/deposit/controller/deposit/deposit.controller.ts
@@ -1,13 +1,20 @@
 import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { depositDto } from '../dtos/deposit.dto';
 import { DepositService } from 'src/services/deposit/deposit.service';
+import { HistoryService } from 'src/services/history/history.service';
+import { Response } from 'express';
 
 @Controller('deposit')
 export class DepositController {
-    constructor(private depositService:DepositService){}
+    constructor(private depositService:DepositService, private HistoryService:HistoryService){}
     @Post()
     @UsePipes(new ValidationPipe())
-    depositMoney(@Body() data:depositDto){
-        this.depositService.depositData(data)
+    async depositMoney(@Body() data:depositDto, res:Response){
+        //get user from token
+        const id = ''
+        await this.depositService.depositData(data)
+        const message = ""
+        await this.HistoryService.insertHistory(id, message)
+        res.send("Done")
     }
 }

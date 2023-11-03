70d907ebe232b0ddfa152a398b69de17dca24043
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-11-03 13:47:27 +0100

    minor changes modular

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,15 +5,17 @@ import { IndexController } from './index/controller/index/index.controller';
 import { UsersService } from './services/users/users.service';
 import { LoginController } from './login/controller/login/login.controller';
 import { WithdrawController } from './withdraw/controller/withdraw/withdraw.controller';
-import { WithdrawService } from './services/withdraw/withdraw.service';
 import { HistoryController } from './history/controller/history/history.controller';
 import { DepositController } from './deposit/controller/deposit/deposit.controller';
 import { DepositService } from './services/deposit/deposit.service';
 import { HistoryService } from './services/history/history.service';
+import { UserResolver } from './resolver/user/user.resolver';
+import { DepositResolver } from './resolver/deposit/deposit.resolver';
+import { HistoryResolver } from './resolver/history/history.resolver';
 
 @Module({
   imports: [SignupModule],
   controllers: [SignupController, IndexController, LoginController, WithdrawController, HistoryController, DepositController],
-  providers: [UsersService, WithdrawService, DepositService, HistoryService],
+  providers: [UsersService, DepositService, HistoryService, UserResolver, DepositResolver, HistoryResolver],
 })
 export class AppModule {}

f8b21bda7973e2dca047f50113979bae3b4447ea
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-11-03 08:34:10 +0100

    mod before graphql

--- server/src/history/controller/history/history.controller.ts
+++ server/src/history/controller/history/history.controller.ts
@@ -1,10 +1,14 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Response } from 'express';
 import { historyDto } from 'src/history/controller/dtos/history.dto';
-
+import { HistoryService } from 'src/services/history/history.service';
 @Controller('history')
 export class HistoryController {
+    constructor(private HistoryService:HistoryService){}
     @Get()
-    getUserHistory(@Body() user:historyDto){
-        
+    async getUserHistory(@Body() user:historyDto, res:Response){
+        //get user from token
+        const history = await this.HistoryService.getHistory(user.id)
+        res.send(history)
     }
 }

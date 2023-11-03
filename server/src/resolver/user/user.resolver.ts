340d70660ad9b28e3c8cd28c5b36148a4f1af835
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-11-03 13:45:38 +0100

    added resolver for user

---
+++ server/src/resolver/user/user.resolver.ts
@@ -0,0 +1,19 @@
+import { Query, Resolver, Mutation, Args } from '@nestjs/graphql';
+import { UsersService } from 'src/services/users/users.service';
+
+@Resolver()
+export class UserResolver {
+    constructor (private readonly UserService:UsersService){}
+    @Mutation()
+    async newUser(user){
+        return await this.UserService.newUser(user)
+    }
+    @Query()
+    async getUser(@Args('email') email:string){
+        return await this.UserService.getUser(email)
+    }
+    @Mutation()
+    async updateUserBalance(@Args("userId") userId:string, @Args("amount") amount:number){
+        return await this.UserService.updateUserBalance(userId, amount)
+    }
+}

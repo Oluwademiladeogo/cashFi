8040df9ac8378aba19bb477ff82d95bf78b8c67c
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-11-03 08:34:42 +0100

    mod before graphql

--- server/src/services/deposit/deposit.service.ts
+++ server/src/services/deposit/deposit.service.ts
@@ -1,8 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { userModel } from 'models/userschema';
 
 @Injectable()
 export class DepositService {
-    depositData(data){
-        //findbybalanceandupdate account balance
+  async depositData(data) {
+    try {
+      const account = await userModel.findOne({ number: data.accountNumber });
+      if (!account)
+        throw new HttpException('Account not found', HttpStatus.BAD_REQUEST);
+      account.balance += data.balance;
+      await account.save();
+      return 'done';
+    } catch (error) {
+      return error;
     }
+  }
 }

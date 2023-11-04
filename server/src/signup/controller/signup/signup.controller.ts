df899a537a5119a623bd40fb130773af31a0f41f
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-11-04 11:07:49 +0100

    Added resolver graphql

--- server/src/signup/controller/signup/signup.controller.ts
+++ server/src/signup/controller/signup/signup.controller.ts
@@ -9,12 +9,12 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { NewUserDto } from './dtos/NewUser.dto';
-import { UsersService } from 'src/services/users/users.service';
 import { Response } from 'express';
 import { userModel } from 'models/userschema';
+import { UserResolver } from 'src/resolver/user/user.resolver';
 @Controller('signup')
 export class SignupController {
-  constructor(private UsersService: UsersService) {}
+  constructor(private UsersResolver: UserResolver) {}
   @Post()
   @UsePipes(new ValidationPipe())
   async SignupUser(@Body() user: NewUserDto, @Res() res: Response) {
@@ -22,7 +22,7 @@ export class SignupController {
     if (userObject)
       throw new HttpException('User already registered', HttpStatus.FOUND);
     try{
-      await this.UsersService.newUser(user)
+      await this.UsersResolver.newUser(user)
     }
     catch(error){
       throw new HttpException("Error saving user details", HttpStatus.REQUEST_TIMEOUT)

dd169f584cfbdaacf98700dd5a59b9b300610cf2
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-11-06 13:21:32 +0100

    updated responses

--- server/src/services/history/history.service.ts
+++ server/src/services/history/history.service.ts
@@ -2,22 +2,34 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { userModel } from 'models/userschema';
 @Injectable()
 export class HistoryService {
-  async getHistory(id) {
-    const user =  await userModel.findOne({ id: id });
-    if(!user) throw new HttpException("User not found", HttpStatus.BAD_REQUEST)
-    return user.history
-  }
-  async insertHistory(id, message: string) {
-    try{
-        const user = await userModel.findOne({ id: id });
-    const history = user.history;
-    history.push(message);
-    user.history = history;
-    await user.save();
-    return 'done';
+  async getHistory(email) {
+    try {
+      const user = await userModel.findOne({ email: email });
+      if (!user)
+        throw new HttpException('Error getting user details', HttpStatus.BAD_REQUEST);
+      return user.history;
+    } catch (error) {
+      throw new HttpException(
+        'Error getting user details',
+        HttpStatus.BAD_REQUEST,
+      );
     }
-    catch(error){
-        return error;
+  }
+  async insertHistory(email: string, message: string ) {
+    try {
+      const user = await userModel.findOne({ email: email });
+      if (!user)
+        throw new HttpException('Error getting user details', HttpStatus.BAD_REQUEST);
+      const history = user.history;
+      history.push(message);
+      user.history = history;
+      await user.save();
+      return true;
+    } catch (error) {
+      throw new HttpException(
+        'Error with history',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 }

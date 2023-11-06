e1051fac77ec82a37e29c98603a53ccaf01ea0d8
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-11-06 13:13:51 +0100

    Edited responses from functions

--- server/src/helpers/auth.helper.ts
+++ server/src/helpers/auth.helper.ts
@@ -1,8 +1,11 @@
 import * as jwt from 'jsonwebtoken';
 import * as bcrypt from 'bcrypt';
+import * as dotenv from 'dotenv';
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { Request, Response } from 'express';
+dotenv.config();
 const authorization = process.env.JWT_SECRET;
 const generateToken = async (user: any) => {
-  console.log(authorization, 'auth');
   const payload = {
     id: user.id,
     name: user.username,
@@ -12,12 +15,30 @@ const generateToken = async (user: any) => {
   const token = await jwt.sign(payload, authorization);
   return token;
 };
-const encrypt = async (password) => {
+const encrypt = async (password: string) => {
   const salt = await bcrypt.genSalt(13);
   const result = await bcrypt.hash(password, salt);
   return { salt, result };
 };
-const compare = async (enteredValue, hashedValue) => {
+const compare = async (enteredValue: string, hashedValue: string) => {
   return await bcrypt.compare(enteredValue, hashedValue);
 };
-export { generateToken, encrypt, compare };
+const getToken = async (req) => {
+  try {
+    if (!req.headers || !req.headers['authorization'])
+      throw new HttpException('Unauthorised', HttpStatus.UNAUTHORIZED);
+    const bearerToken = req.headers['authorization']?.split(' ')[1];
+    return bearerToken;
+  } catch (error) {
+    throw new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+};
+const getTokenPayload = async (bearerToken: string) => {
+  try {
+    const payload = await jwt.verify(bearerToken, process.env.JWT_SECRET);
+    return payload;
+  } catch (error) {
+    throw new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+};
+export { generateToken, encrypt, compare, getToken, getTokenPayload };

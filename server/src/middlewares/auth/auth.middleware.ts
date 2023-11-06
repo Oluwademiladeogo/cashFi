5e5d98a05fdad51d7e1958e33bf49d20b1f06fa3
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-11-06 13:17:49 +0100

    bug fix

--- server/src/middlewares/auth/auth.middleware.ts
+++ server/src/middlewares/auth/auth.middleware.ts
@@ -1,13 +1,19 @@
 import { HttpException, HttpStatus, Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 import * as jwt from "jsonwebtoken"
+import * as dotenv from 'dotenv'
+dotenv.config()
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
+  use(req: Request, res: Response, next:NextFunction) {
     if(!req.headers.authorization) throw new HttpException("Unauthorised", HttpStatus.UNAUTHORIZED)
     const bearerToken = req.headers.authorization?.split(' ')[1];
-    const {error} = jwt.verify(bearerToken, process.env.JWT_SECRET)
-    if(error) throw new HttpException("Unauthorised", HttpStatus.UNAUTHORIZED)
-    next();
+  next()
+try{
+  const token = jwt.verify(bearerToken, process.env.JWT_SECRET)
+  return token
+}catch(error){
+  throw new HttpException("Unauthorised", HttpStatus.UNAUTHORIZED)
+}   
   }
 }

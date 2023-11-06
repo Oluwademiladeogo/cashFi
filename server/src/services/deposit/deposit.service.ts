b2fbe90c0200bec7751f38e3ec330930ff23e5eb
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-11-06 13:20:13 +0100

    updated responses

--- server/src/services/deposit/deposit.service.ts
+++ server/src/services/deposit/deposit.service.ts
@@ -3,15 +3,11 @@ import { userModel } from 'models/userschema';
 @Injectable()
 export class DepositService {
   async depositData(data) {
-    try {
-      const account = await userModel.findOne({ number: data.accountNumber });
-      if (!account)
-        throw new HttpException('Account not found', HttpStatus.BAD_REQUEST);
-      account.balance += data.balance;
-      await account.save();
-      return 'done';
-    } catch (error) {
-      return error;
-    }
+    const account = await userModel.findOne({ number: data.number });
+    if (!account)
+      throw new HttpException('Account not found', HttpStatus.BAD_REQUEST);
+    account.balance += data.amount;
+    await account.save();
+    return true;
   }
 }

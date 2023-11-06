b7327a4974c7975762a3bc1ccbf22816e77fb369
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-11-06 13:14:43 +0100

    edited responses

--- server/src/history/controller/history/history.controller.ts
+++ server/src/history/controller/history/history.controller.ts
@@ -1,14 +1,19 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
 import { Response } from 'express';
-import { historyDto } from 'src/history/controller/dtos/history.dto';
+import { getToken, getTokenPayload } from 'src/helpers/auth.helper';
 import { HistoryResolver } from 'src/resolver/history/history.resolver';
 @Controller('history')
 export class HistoryController {
-    constructor(private HistoryResolver:HistoryResolver){}
-    @Get()
-    async getUserHistory(@Body() user:historyDto, res:Response){
-        //get user from token
-        const history = await this.HistoryResolver.getHistory(user.id)
-        res.send(history)
+  constructor(private HistoryResolver: HistoryResolver) {}
+  @Get()
+  async getUserHistory(@Req() req: Request, @Res() res: Response) {
+    const token = await getToken(req);
+    const payload = await getTokenPayload(token);
+    const history = await this.HistoryResolver.getHistory(payload.email);
+    if (history[0]) {
+      res.status(200).send(history);
+    } else {
+      res.status(200).send('Nothing in history');
     }
+  }
 }

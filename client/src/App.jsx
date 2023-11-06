02026eadb57960651036e5a04d09b3df5e9bf0a7
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-11-06 19:41:52 +0100

    Implemented react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,28 @@
-import { Route, Routes } from 'react-router-dom';
+import React from "react";
 import Login from './components/Login/Login';
 import Signup from './components/Signup/Signup';
 import Dashboard from './components/Dashboard/Dashboard';
+import Transfer from './components/Transfer/Transfer';
+import History from './components/History/History';
+import Deposit from './components/Deposit/Deposit';
+import Withdraw from './components/Withdraw/Withdraw';
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/history" element={<History />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        {/* route index from backend thats if it tries to get backend it redirects them to either login or dashboard based on token */}
+        <Route path="/signup" element={<Signup />} />{" "}
+        <Route path="/login" element={<Login />} />
+        <Route path="/transfer" element={<Transfer />} />
+        <Route path="/withdraw" element={<Withdraw />} />
+        <Route path="/deposit" element={<Deposit />} />
+
       </Routes>
-    </>
+      </>
   );
-}
+};
 
 export default App;

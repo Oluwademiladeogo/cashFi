88e5fa3e30e2d849a1a59ad2e14c2414e85a628d
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-11-06 13:58:13 +0100

    enabled cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,5 +5,10 @@ async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
 }
 bootstrap();

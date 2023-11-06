57f9b9191d16f0faa791cf4620092c23d87a7126
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-11-06 19:41:02 +0100

    Updated Navbar import

--- client/src/components/Withdraw/Withdraw.jsx
+++ client/src/components/Withdraw/Withdraw.jsx
@@ -1,7 +1,65 @@
-import "./Withdraw.css"
-const Withdraw = ()=>{
-    return (
-        <></>
-    )
-}
-export default Withdraw
+import React from 'react';
+import './Withdraw.css';
+import Navbar from '../Navbar/Navbar';
+
+const Withdraw = () => {
+  const [pin, setPin] = React.useState('');
+  const [amount, setAmount] = React.useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch('http://localhost:3000/withdraw', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        pin,
+        amount,
+      }),
+    });
+
+    if (response.status === 200) {
+      // Withdrawal successful
+    } else {
+      // Withdrawal failed
+    }
+  };
+
+  return (
+    <>
+      <Navbar />
+
+      <div className="withdraw-page">
+        <form className="withdraw-form" onSubmit={handleSubmit}>
+          <div className="title">
+            <h1>Withdraw</h1>
+          </div>
+          <div className="form-input"></div>
+          <div className="form-input">
+            <input
+              className="input pin"
+              type="password"
+              placeholder="PIN"
+              name="pin"
+              onChange={(e) => setPin(e.target.value)}
+            />
+          </div>
+          <div className="form-input">
+            <input
+              className="input amount"
+              type="number"
+              placeholder="Amount"
+              name="amount"
+              onChange={(e) => setAmount(e.target.value)}
+            />
+          </div>
+          <button>withdraw</button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Withdraw;

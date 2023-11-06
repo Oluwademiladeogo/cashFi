46120fd001dbbd7e8512deb41ec5ad9e9fca2f60
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-11-06 13:21:40 +0100

    updated responses

--- server/src/services/users/users.service.ts
+++ server/src/services/users/users.service.ts
@@ -4,31 +4,59 @@ import { userModel } from 'models/userschema';
 @Injectable()
 export class UsersService {
   async newUser(user) {
-    const passwordData = await encrypt(user.password);
-    const pinData = await encrypt(user.pin);
-    const createUser = new userModel({
-      username: user.username,
-      email: user.email,
-      number: user.number,
-      pin: pinData.result,
-      pinSalt: pinData.salt,
-      password: passwordData.result,
-      passwordSalt: passwordData.salt,
-      balance: 0,
-    });
-    await createUser.save();
+    try {
+      const passwordData = await encrypt(user.password);
+      const pinData = await encrypt(user.pin);
+      const createUser = new userModel({
+        username: user.username,
+        email: user.email,
+        number: user.number,
+        pin: pinData.result,
+        pinSalt: pinData.salt,
+        password: passwordData.result,
+        passwordSalt: passwordData.salt,
+        balance: 0,
+      });
+      await createUser.save();
+    } catch (error) {
+      throw new HttpException('Internal server error', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
   async getUser(email: string) {
-    return await userModel.findOne({ email: email });
+    try {
+      return await userModel.findOne({ email: email });
+    } catch (error) {
+      throw new HttpException(
+        'Error getting user details',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+  async getUserByNumber(number: number) {
+    try {
+      return await userModel.findOne({ number: number });
+    } catch (error) {
+      throw new HttpException(
+        'Error getting user details',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
   async updateUserBalance(userId: string, amount: number) {
-    const user = await userModel.findById(userId);
-    if (!user)
+    try {
+      const user = await userModel.findById(userId);
+      if (!user)
+        throw new HttpException(
+          'Internal server error',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      user.balance = amount;
+      await user.save();
+    } catch (error) {
       throw new HttpException(
-        'Error querying db',
-        HttpStatus.EXPECTATION_FAILED,
+        'Internal server error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
       );
-    user.balance = amount;
-    await user.save();
+    }
   }
 }

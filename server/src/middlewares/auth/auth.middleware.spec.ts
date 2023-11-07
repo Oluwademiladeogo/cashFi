a03c92654f94604dae438dcbd2b18da14e614a03
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-11-07 10:16:46 +0100

    Jest test

--- server/src/middlewares/auth/auth.middleware.spec.ts
+++ server/src/middlewares/auth/auth.middleware.spec.ts
@@ -1,7 +1,17 @@
 import { AuthMiddleware } from './auth.middleware';
+import { HttpException, HttpStatus } from '@nestjs/common';
+import * as jwt from 'jsonwebtoken';
 
 describe('AuthMiddleware', () => {
-  it('should be defined', () => {
-    expect(new AuthMiddleware()).toBeDefined();
+  let middleware: AuthMiddleware;
+  let nextSpy: jest.Mock;
+
+  beforeEach(() => {
+    middleware = new AuthMiddleware();
+    nextSpy = jest.fn();
+  });
+
+  it('should throw an error if the middleware is called', () => {
+    expect(() => middleware.use(null, null, nextSpy)).toThrow(new HttpException('Unauthorised', HttpStatus.UNAUTHORIZED));
   });
 });

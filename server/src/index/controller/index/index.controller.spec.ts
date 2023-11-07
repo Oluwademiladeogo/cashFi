ba0c8e25e13b2498d3a94ff2cb57c52b8394c0e4
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-11-07 10:16:28 +0100

    Jest test

--- server/src/index/controller/index/index.controller.spec.ts
+++ server/src/index/controller/index/index.controller.spec.ts
@@ -1,8 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { IndexController } from './index.controller';
+import { MockResponse } from 'jest-express-mock';
 
 describe('IndexController', () => {
   let controller: IndexController;
+  let mockResponse: MockResponse;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -10,9 +12,17 @@ describe('IndexController', () => {
     }).compile();
 
     controller = module.get<IndexController>(IndexController);
+    mockResponse = new MockResponse();
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should redirect to the dashboard page', async () => {
+    // Call the getIndex method
+    await controller.getIndex(mockResponse);
+
+    // Expect the response status code to be 302 (redirect)
+    expect(mockResponse.status).toBe(302);
+
+    // Expect the response location header to be set to the dashboard page
+    expect(mockResponse.headers).toHaveProperty('location', '/dashboard');
   });
 });

c402eb86c42cdacc8b35bc823db5d9aa3c7239c1
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-11-07 10:16:20 +0100

    Jest test

--- server/src/history/controller/history/history.controller.spec.ts
+++ server/src/history/controller/history/history.controller.spec.ts
@@ -1,18 +1,26 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { HistoryController } from './history.controller';
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { getMockReq, getMockRes } from '@jest-mock/express';
+import { Response } from 'express';
+import { getToken, getTokenPayload } from 'src/helpers/auth.helper';
+import { HistoryResolver } from 'src/resolver/history/history.resolver';
 
-describe('HistoryController', () => {
-  let controller: HistoryController;
+@Controller('history')
+export class HistoryController {
+  constructor(private HistoryResolver: HistoryResolver) {}
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [HistoryController],
-    }).compile();
+  @Get()
+  async getUserHistory(@Req() req: Request, @Res() res: Response) {
+    const token = await getToken(req);
+    const payload = await getTokenPayload(token);
+    const history = await this.HistoryResolver.getHistory(payload.email);
 
-    controller = module.get<HistoryController>(HistoryController);
-  });
-
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+    // Mock the response status code and send the history data
+    if (history[0]) {
+      res.statusCode = 200;
+      res.send(history);
+    } else {
+      res.statusCode = 200;
+      res.send('Nothing in history');
+    }
+  }
+}

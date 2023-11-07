07372feb6e7e450ad01d029cb4b217cb129b2516
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-11-07 10:17:24 +0100

    Jest test

--- server/src/transfer/controller/transfer/transfer.controller.spec.ts
+++ server/src/transfer/controller/transfer/transfer.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TransferController } from './transfer.controller';
+import { transferDto } from '../dtos/transfer.dto';
 
 describe('TransferController', () => {
   let controller: TransferController;
@@ -12,7 +13,16 @@ describe('TransferController', () => {
     controller = module.get<TransferController>(TransferController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should transfer money if the data is valid', async () => {
+    const data = {
+      number: '1234567890',
+      amount: 1000,
+      pin: '123456',
+    };
+
+    // await controller.transferMoney(data, data.amount, data.pin);
+
+    // Expect the controller to have been called with the correct arguments
+    expect(controller.transferMoney).toHaveBeenCalledWith(data);
   });
 });
